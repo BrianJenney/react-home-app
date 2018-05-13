a250a2458fab8aa3697fdb3e5f188c7870f46c77
BrianJenney <brianjenney83@gmail.com>
2018-05-13 12:47:54 -0700

    show monthly mortgage

--- src/components/modals/Message.jsx
+++ src/components/modals/Message.jsx
@@ -1,86 +1,84 @@
-import React from 'react';
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
-import TextField from 'material-ui/TextField';
+import React from "react";
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+import TextField from "material-ui/TextField";
 
-import API from '../../api/helpers.js';
+import API from "../../api/helpers.js";
 
-import '../../styles/messages.css';
+import "../../styles/messages.css";
 
 export default class DialogModal extends React.Component {
-
-  constructor(props){
-    super(props)
-    this.state= {
-      disabled: false,
-      userMessage: '',
-      recipientEmail: null,
-      senderEmail: null,
-      propertyId: null
-    };
-  };
-
-  componentWillReceiveProps=(nextProps)=>{
-    this.setState({
-      recipientEmail: nextProps.propertyInfo.email, 
-      senderEmail: nextProps.senderEmail,
-      propertyId: nextProps.propertyInfo._id
-    });
-  };
-
-  handleClose = () => {
-    this.props.closeModal();
-  };
-
-  textChange=(e)=>{
-    this.setState({userMessage: e.target.value});
-  };
-
-  submitMessage=()=>{
-    const message={
-      id: this.state.propertyId,
-      text: this.state.userMessage,
-      from: this.props.senderEmail,
-      to: this.state.recipientEmail
+    constructor(props) {
+        super(props);
+        this.state = {
+            disabled: false,
+            userMessage: "",
+            recipientEmail: null,
+            senderEmail: null,
+            propertyId: null
+        };
     }
 
-    API.postMessage(message).then((response)=>{
-      this.setState({userMessage: ''});
-      this.props.closeModal();
-    });
-  };
+    componentWillReceiveProps = nextProps => {
+        this.setState({
+            recipientEmail: nextProps.propertyInfo.email,
+            senderEmail: nextProps.senderEmail,
+            propertyId: nextProps.propertyInfo._id
+        });
+    };
 
-  render() {
-    const actions = [
-      <TextField
-        value={this.state.userMessage}
-        onChange={this.textChange.bind(this)}
-        hintText="Send a message"
-        fullWidth={true}
-      />,
-      <FlatButton
-        label="Close"
-        primary={true}
-        onClick={this.handleClose}
-      />,
-      <FlatButton
-        label="Send"
-        disabled={!this.state.userMessage.length}
-        primary={true}
-        onClick={this.submitMessage}
-      />,
-    ];
+    handleClose = () => {
+        this.props.closeModal();
+    };
 
-    return (
-      <div>
-        <Dialog
-          title="Message Owner"
-          actions={actions}
-          modal={true}
-          open={this.props.open}
-        >
-        </Dialog>
-      </div>
-    );
-  }
+    textChange = e => {
+        this.setState({ userMessage: e.target.value });
+    };
+
+    submitMessage = () => {
+        const message = {
+            id: this.state.propertyId,
+            text: this.state.userMessage,
+            from: this.props.senderEmail,
+            to: this.state.recipientEmail
+        };
+
+        API.postMessage(message).then(response => {
+            this.setState({ userMessage: "" });
+            this.props.closeModal();
+        });
+    };
+
+    render() {
+        const actions = [
+            <TextField
+                value={this.state.userMessage}
+                onChange={this.textChange.bind(this)}
+                hintText="Send a message"
+                fullWidth={true}
+            />,
+            <FlatButton
+                label="Close"
+                primary={true}
+                onClick={this.handleClose}
+            />,
+            <FlatButton
+                label="Send"
+                disabled={!this.state.userMessage.length}
+                primary={true}
+                onClick={this.submitMessage}
+            />
+        ];
+
+        return (
+            <div>
+                <Dialog
+                    title="Message Owner"
+                    actions={actions}
+                    modal={true}
+                    open={this.props.open}
+                />
+            </div>
+        );
+    }
 }
--- src/pages/MyMessages/UserMessage.jsx
+++ src/pages/MyMessages/UserMessage.jsx
@@ -1,116 +1,130 @@
+import React from "react";
+import API from "../../api/helpers.js";
+import NavBar from "../../components/NavBar";
+import Divider from "material-ui/Divider";
+import ChatBox from "./components/ChatBox";
+import { List, ListItem } from "material-ui/List";
 
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
-import React from 'react';
-import API from '../../api/helpers.js';
-import NavBar from '../../components/NavBar';
-import Divider from 'material-ui/Divider';
-import ChatBox from './components/ChatBox';
-import {List, ListItem} from 'material-ui/List';
+import * as loginActions from "../../actions/login";
+import * as logoutActions from "../../actions/logout";
 
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-
-import * as loginActions from '../../actions/login';
-import * as logoutActions from '../../actions/logout';
-
-class UserMessages extends React.Component{
-
-    constructor(props){
-        super(props)
+class UserMessages extends React.Component {
+    constructor(props) {
+        super(props);
         this.state = {
             open: false,
-            picID: '',
-            messages:[],
-            recipient: '',
+            picID: "",
+            messages: [],
+            recipient: "",
             convo: []
         };
 
         this.refreshConvo = this.refreshConvo.bind(this);
-    };
+    }
 
-    componentDidMount=()=>{
+    componentDidMount = () => {
         let self = this;
-        API.getMessages(this.props.email).then((response)=>{
-
+        API.getMessages(this.props.email).then(response => {
             const messages = response.data;
 
             //only show the user in the message that is not the logged in user
-            messages.map((message)=>{
-                return(
-                    message.participants = message.participants.filter((name)=>{
+            messages.map(message => {
+                return (message.participants = message.participants.filter(
+                    name => {
                         return name !== self.props.email;
-                    })
-                )
-            })
+                    }
+                ));
+            });
 
-            return self.setState({messages: response.data});
+            return self.setState({ messages: response.data });
         });
     };
 
-    getConvo=(message)=>{
-        this.setState({convo: message.messages, recipient: message.participants[0], picID: message.id});
+    getConvo = message => {
+        this.setState({
+            convo: message.messages,
+            recipient: message.participants[0],
+            picID: message.id
+        });
     };
 
-    refreshConvo=(recipient, sender)=>{
-        API.getConvo(recipient, sender).then((response)=>{
-            this.setState({convo: response.data[0].messages, recipient, picId: response.data[0].id})
-        })
-    }
+    refreshConvo = (recipient, sender) => {
+        API.getConvo(recipient, sender).then(response => {
+            this.setState({
+                convo: response.data[0].messages,
+                recipient,
+                picId: response.data[0].id
+            });
+        });
+    };
 
-    render(){
-        return(          
+    render() {
+        return (
             <div>
-                <NavBar selectedIndex={3}/>
+                <NavBar selectedIndex={3} />
                 <div className="row">
-                    <div className="text-center"> 
+                    <div className="text-center">
                         <h1 className="text-center">My Messages</h1>
                     </div>
                     <div className="row">
-                    {this.state.messages.map((message, i)=>{
-                        return(
-                        <div key={i}>
-                                <div className="col-md-4">
-                                    <List>
-                                        <ListItem primaryText={message.participants[0]} 
-                                        onClick={this.getConvo.bind(this, message)}
-                                        leftIcon={<img src="https://cdn2.iconfinder.com/data/icons/business-and-finance-related-hand-gestures/256/face_female_blank_user_avatar_mannequin-512.png" 
-                                            width="25%"
-                                            alt=""/>} />
-                                    </List>
-                                <Divider/>
+                        {this.state.messages.map((message, i) => {
+                            return (
+                                <div key={i}>
+                                    <div className="col-md-4">
+                                        <List>
+                                            <ListItem
+                                                primaryText={
+                                                    message.participants[0]
+                                                }
+                                                onClick={this.getConvo.bind(
+                                                    this,
+                                                    message
+                                                )}
+                                                leftIcon={
+                                                    <img
+                                                        src="https://cdn2.iconfinder.com/data/icons/business-and-finance-related-hand-gestures/256/face_female_blank_user_avatar_mannequin-512.png"
+                                                        width="25%"
+                                                        alt=""
+                                                    />
+                                                }
+                                            />
+                                        </List>
+                                        <Divider />
+                                    </div>
                                 </div>
-                            
+                            );
+                        })}
+                        <div className="col-md-8">
+                            <ChatBox
+                                refreshConvo={this.refreshConvo}
+                                picID={this.state.picID}
+                                recipient={this.state.recipient}
+                                sender={this.props.email}
+                                open={this.state.open}
+                                messages={this.state.convo}
+                            />
                         </div>
-                        )
-                    })}
-                    <div className="col-md-8">
-                        <ChatBox
-                        refreshConvo={this.refreshConvo}
-                        picID={this.state.picID}
-                        recipient={this.state.recipient}
-                        sender={this.props.email}
-                        open={this.state.open}
-                        messages={this.state.convo}/>
-                    </div>
                     </div>
                 </div>
-            </div>            
-        )
+            </div>
+        );
     }
-
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
     return {
         id: state.loggedIn.id,
         email: state.loggedIn.name
     };
-};
+}
 
 function mapDispatchToProps(dispatch) {
     return {
         loginaction: bindActionCreators(loginActions, dispatch),
         logoutaction: bindActionCreators(logoutActions, dispatch)
-    }
-};
+    };
+}
 export default connect(mapStateToProps, mapDispatchToProps)(UserMessages);
