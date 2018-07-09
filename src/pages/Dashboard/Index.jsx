173b8e901aaea59fcbca55564cdbcf3aec9a1fc3
jenneyb <brianjenney83@gmail.com>
2018-07-08 18:18:43 -0700

    Feature/dashboard skeleton (#9)
    * WIP
    
    * skeleton for dashboard
    
    * revert changes on add prop page

---
+++ src/pages/Dashboard/Index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import * as loginActions from "../../actions/login";
+import * as logoutActions from "../../actions/logout";
+import "../../styles/dashboard.css";
+
+import Profile from "./components/Profile";
+
+import API from "../../api/helpers.js";
+
+class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h1>Dashboard</h1>
+                <Profile />
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        id: state.loggedIn.id,
+        email: state.loggedIn.name
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        loginaction: bindActionCreators(loginActions, dispatch),
+        logoutaction: bindActionCreators(logoutActions, dispatch)
+    };
+}
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Dashboard);
