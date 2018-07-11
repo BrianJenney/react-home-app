d4c569dfcc5610ec00bafbfc54df6a8484124f46
Tony Carbetta <ruquss@gmail.com>
2018-07-11 14:38:33 -0700

    rough draft of html and css for /dashboard FILL OUT YOUR PROFILE area, stills need some more javascript functionality and link to back end

--- src/pages/Dashboard/components/Profile.jsx
+++ src/pages/Dashboard/components/Profile.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Collapse, Button, CardBody, Card } from "reactstrap";
+import ListHome from "./ListHome";
+import FillOutProfile from "./FillOutProfile";
 
 class Profile extends React.Component {
     constructor(props) {
@@ -20,28 +22,8 @@ class Profile extends React.Component {
     render() {
         return (
             <div>
-                <div style={{ display: "flex", alignItems: "center" }}>
-                    <h1 className="pr-4">1</h1>
-                    <span className="section-title">List Home</span>
-                    <span
-                        className={
-                            this.state.collapse
-                                ? "fa fa-2x fa-angle-up pl-4"
-                                : "fa fa-2x fa-angle-down pl-4"
-                        }
-                        onClick={this.toggle}
-                    />
-                </div>
-                <Collapse isOpen={this.state.collapse}>
-                    <Card>
-                        <CardBody>
-                            Anim pariatur cliche reprehenderit, enim eiusmod
-                            high life accusamus terry richardson ad squid. Nihil
-                            anim keffiyeh helvetica, craft beer labore wes
-                            anderson cred nesciunt sapiente ea proident.
-                        </CardBody>
-                    </Card>
-                </Collapse>
+                <ListHome />
+                <FillOutProfile />
             </div>
         );
     }
