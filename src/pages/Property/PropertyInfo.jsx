44137e60d4a5da41ee40b9d9a8a42a641097e868
ShuangjiaoHu <shu16@dons.usfca.edu>
2018-08-24 13:54:45 -0700

    keep with staging

--- src/pages/Property/PropertyInfo.jsx
+++ src/pages/Property/PropertyInfo.jsx
@@ -3,7 +3,6 @@ import NavBar from "../../components/BreadcrumbNav";
 import TopNav from "../../components/TopNav";
 import API from "../../api/helpers";
 import moment from "moment";
-import { Link } from "react-router-dom";
 import "../../styles/propertyInfo.css";
 import DialogModal from "../../components/modals/Message";
 
@@ -11,6 +10,7 @@ import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as loginActions from "../../actions/login";
 import * as logoutActions from "../../actions/logout";
+import TextField from '@material-ui/core/TextField';
 
 const FontAwesome = require("react-fontawesome");
 
@@ -21,17 +21,30 @@ class Property extends Component {
         monthlyPayment: 0,
         open: false,
         messages: [],
-        user: null
+        user: null,
+        imgs: [],
+        location: []
     };
 
     componentDidMount = () => {
+        let imgs = [];
         API.getHome(this.props.match.params.id).then(response => {
+            console.log(response);
+            
+            response.data.doc[0].imgs.forEach( img => {
+                imgs.push(img);
+            })
             this.setState({
+                location: response.data.doc[0].location,
                 mainImg: response.data.doc[0].imgs[0],
                 property: response.data.doc[0],
                 monthlyPayment: response.data.monthly,
                 user: response.data.user
             });
+        }).then(() => {
+            this.setState({
+                imgs : imgs
+            })
         });
     };
 
@@ -45,89 +58,116 @@ class Property extends Component {
 
     render() {
         return (
-            <div>
+            <div className="property-info">
                 <TopNav />
                 {this.state &&
                     this.state.property && (
-                        <div style={{ marginBottom: 200 }}>
-                            <div className="row">
-                                <div className="col-7">
+                        <div className="upper" style={{ marginBottom: 200 }}>
+                            <div className="row" style={{ height:600 }}>
+                                <div className="col-8">
                                     <img
+                                        style={{ height:620 }}
                                         className="img-fluid details-main-img"
                                         src={this.state.mainImg}
                                         alt={this.state.property._id}
                                     />
                                 </div>
-                                <div className="col-5 contact">
+                                <div className="col-4 contact">
                                     <div className="contact-actions">
                                         <h2>${this.state.property.price}</h2>
-                                        <p className="text-muted">
+                                        <p className="text-muted" style={{marginBottom:30}}>
                                             ${this.state.monthlyPayment}/mo (Est
                                             Mortgage)
                                         </p>
-
-                                        <button className="btn btn-default btn-offer">
-                                            <Link
-                                                to={`/buyerdashboard/${
-                                                    this.state.property._id
-                                                }`}
-                                            >
-                                                Make an Offer
-                                            </Link>
-                                        </button>
-                                        <div className="btn-toolbar">
-                                            <button className="btn btn-default">
-                                                Get Prequalified
+                                        <div className="contact-buttons" >                                        
+                                            <button className="btn btn-default btn-offer btn-long">
+                                                Send Offer
                                             </button>
-                                            <button
-                                                className="btn btn-default"
-                                                onClick={this.openMessage}
-                                            >
-                                                Message Owner
+                                            <button className="btn btn-default btn-long">
+                                                Request Disclosure Package
                                             </button>
+                                            <div className="btn-toolbar btn-two">
+                                                <button className="btn btn-default btn-short">
+                                                    Get Prequalified
+                                                </button>
+                                                <button className="btn btn-default btn-short" onClick={this.openMessage}>
+                                                    Message Owner
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div style={{marginTop:30}}>
+                                            <FontAwesome name='calendar' style={{color:"#355d91" , padding:12}}/>
+                                            Set Appointment
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
-                            <div className="details">
-                                <div className="address-details">
-                                    <h3>
-                                        {this.state.property.address},{" "}
-                                        {this.state.property.city}{" "}
-                                        {this.state.property.state},{" "}
-                                        {this.state.property.zipCode}
-                                    </h3>
-                                </div>
-                                <div className="property-details">
-                                    <span>
-                                        <FontAwesome name="bed" />
-                                        {this.state.property.bedRooms} beds
-                                    </span>
-                                    <span>
-                                        <FontAwesome name="bath" />
-                                        {this.state.property.bathRooms} baths
-                                    </span>
-                                    <span>
-                                        <FontAwesome name="crop" />
-                                        {this.state.property.sqFeet} sq ft
-                                    </span>
-                                    <span>
-                                        <FontAwesome name="wrench" />
-                                        {moment(
-                                            this.state.property.yearBuilt
-                                        ).get("year")}
-                                    </span>
-                                    <span className="details-property-type">
-                                        <FontAwesome name="home" />
-                                        {this.state.property.propertyType}
-                                    </span>
-                                </div>
-                                <div className="description">
-                                    <p>{this.state.property.description}</p>
+                            <hr style={{ marginTop: 20.5 }} />
+                            <div className="lower">
+                                <div className="details row" >
+                                    <div className="col-10">
+                                        <div className="address-details">
+                                            <h3>
+                                                {this.state.property.address},{" "}
+                                                {this.state.property.city}{" "}
+                                                {this.state.property.state},{" "}
+                                                {this.state.property.zipCode}
+                                            </h3>
+                                        </div>
+                                        <div className="property-details">                                        
+                                            <span>
+                                                <FontAwesome className="detail-icon" name="bed" size='2x'/>
+                                                {this.state.property.bedRooms} beds
+                                            </span>
+                                            <span>
+                                                <FontAwesome className="detail-icon" name="bath" size='2x'/>
+                                                {this.state.property.bathRooms} baths
+                                            </span>
+                                            <span>
+                                                <FontAwesome className="detail-icon" name="crop" size='2x'/>
+                                                {this.state.property.sqFeet} sq ft
+                                            </span>
+                                            <span>
+                                                <FontAwesome className="detail-icon" name="crop" size='2x'/>
+                                                {this.state.property.sqFeet} sq ft
+                                            </span>
+                                            <span>
+                                                <FontAwesome className="detail-icon" name="wrench" size='2x'/>
+                                                {moment(
+                                                    this.state.property.yearBuilt
+                                                ).get("year")}
+                                            </span>
+                                            <span className="details-property-type">
+                                                <FontAwesome name="home" size='2x'/>
+                                                {this.state.property.propertyType}
+                                            </span>
+                                        
+                                        </div>
+                                        <div className="description"  >
+                                            <p>{this.state.property.description}</p>
+                                        </div>
+                                    </div>
+                                    <div className="col-2">
+                                        <img
+                                            className="house-map"
+                                            src={this.state.mainImg}
+                                            alt={this.state.property._id}
+                                        />
+                                    </div>
                                 </div>
                             </div>
+                            <div className="image-list">
+                                <FontAwesome className="image-icon" name='heart-o' size='2x' />
+                                {this.state.imgs.map(function(img, index) {
+                                    return (
+                                        <label key={index}>
+                                            <img src={img}/>
+                                        </label>
+                                    );
+                                })}                                
+                            </div>
                         </div>
+                        
                     )}
                 <DialogModal
                     closeModal={this.closeModal.bind(this)}
@@ -155,6 +195,7 @@ function mapDispatchToProps(dispatch) {
         logoutaction: bindActionCreators(logoutActions, dispatch)
     };
 }
+
 export default connect(
     mapStateToProps,
     mapDispatchToProps
