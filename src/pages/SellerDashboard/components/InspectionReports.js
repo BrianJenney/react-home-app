c9d9f72c452550d2b70b2658b400a740b31daae0
BrianJenney <brianjenney83@gmail.com>
2021-05-09 10:22:47 -0700

    update file to trigger deployment

--- src/pages/SellerDashboard/components/InspectionReports.js
+++ src/pages/SellerDashboard/components/InspectionReports.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import DashboardItem from '../../../components/DashboardItem';
 import API from '../../../api/helpers';
 import '../../../styles/listingDocuments.css';
